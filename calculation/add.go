ad8bbbaf83ab22c368fb8000b763fdb5553682aa
unknownunnamed <fikriar19@student.untan.ac.id>
2021-12-04 22:11:16 +0700

    add private function add in package calculation, that function only can call in package calculation

--- calculation/add.go
+++ calculation/add.go
@@ -1,5 +1,9 @@
 package calculation
 
 func Add(numberOne int, numberTwo int) int {
+	return add(numberOne, numberTwo)
+}
+
+func add(numberOne int, numberTwo int) int {
 	return numberOne + numberTwo
 }
